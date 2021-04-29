a0d81b7e3b935e053c48787a7e154e7e7b8bec87
DataCrusade1999 <ashutosh.pandeyhlr007@gmail.com>
2021-04-29 12:12:22 +0530

    Added another response

--- test.py
+++ test.py
@@ -2,20 +2,19 @@ import os
 import openai
 
 # Load your API key from an environment variable or secret management service
+openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 def gpt3(stext):
-
-    openai.api_key = os.environ.get("OPENAI_API_KEY")
 
     response = openai.Completion.create(engine="davinci-instruct-beta", prompt=stext, max_tokens=1000)
 
     return response.choices[0].text
 
-query = 'Tell me about ABESIT College in India'
+query = 'What are your views on Existentialism?'
 
 response = gpt3(query)
 
-with open("GPT34.txt","a") as f:
+with open("GPT35.txt","a") as f:
     f.write(query)
     f.write(response)
    
