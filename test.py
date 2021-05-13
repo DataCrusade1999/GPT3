7c48a18b8f7833a90cb95d478e149dcdd770b1c6
DataCrusade1999 <ashutosh.pandeyhlr007@gmail.com>
2021-05-13 14:55:11 +0530

    added query

--- test.py
+++ test.py
@@ -6,16 +6,16 @@ openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 def gpt3(stext):
 
-    response = openai.Completion.create(engine="davinci", prompt=stext, max_tokens=1000, temperature=0.34, 
-    frequency_penalty=0.31, presence_penalty=0.23, top_p=0.5)
+    response = openai.Completion.create(engine="davinci", prompt=stext, max_tokens=1000, temperature=0.58, 
+    frequency_penalty=0.39, presence_penalty=0.32, top_p=1)
 
     return response.choices[0].text
 
-query = 'What are your views on the Anthropocene epoch?'
+query = 'What is the meaning of life?'
 
 response = gpt3(query)
 
-with open("GPT36.txt","a") as f:
+with open("GPT37.txt","a") as f:
     f.write(query)
     f.write(response)
    
